7d5147897bd7af86981c3de515f5f920a751f39d
Felipe Augustos <felipe_augustos84@outlook.com.br>
2022-08-07 02:18:31 -0300

    feat: finsihin first challenge

--- challenge-1.js
+++ challenge-1.js
@@ -1,8 +1,18 @@
 // Desafio 1: FizzBuzz
 
 function fizzBuzz(number) {
-  // Write your code here
+	const arr = [...Array(number).keys()].map(n => ++n).map(n => {
+		let number
+
+		number = n % 3 ? n : 'Fizz'
+		number = n % 5 ? number : 'Buzz'
+		number = (n % 3 || n % 5) ? number : 'FizzBuzz'
+
+		return number
+	})
+
+	return arr
 }
 
-console.log(fizzBuzz(15));
+console.log(JSON.stringify(fizzBuzz(15)));
 // ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
