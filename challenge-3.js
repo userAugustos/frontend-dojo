eb5c5765bfa07d4cd8eece7852e6dd1363f589ab
Felipe Augustos <felipe_augustos84@outlook.com.br>
2022-08-07 12:56:51 -0300

    feat: finished third challenge

--- challenge-3.js
+++ challenge-3.js
@@ -1,7 +1,21 @@
 // Desafio 3: Duplicate
 
 function duplicateCount(text) {
-  // Write your code here
+	const arr = text.toLowerCase().split('')
+	let valuesSoFar = Object.create(null)
+	let caracteres = []
+
+	for (let i = 0; i < arr.length; i++) {
+		if(valuesSoFar[arr[i]]){
+			caracteres.push(arr[i])
+		}
+		valuesSoFar[arr[i]] = true
+	}
+
+	return {
+		duplicados: caracteres.length,
+		caracteres
+	}
 }
 
 console.log(duplicateCount("abcde")); // {duplicados: 0, caracteres: []}
